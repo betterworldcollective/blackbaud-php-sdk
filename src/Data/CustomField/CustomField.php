59a423668c05c64283ba173f923706d98589a464
marielsabornido <marielsabornido@gmail.com>
2025-07-24 14:30:19 +0800

    Update custom field type

--- src/Data/CustomField/CustomField.php
+++ src/Data/CustomField/CustomField.php
@@ -4,6 +4,7 @@ namespace Blackbaud\Data\CustomField;
 
 use Blackbaud\Contracts\Data;
 use Blackbaud\Data\BaseData;
+use Blackbaud\Enums\CustomFieldCategoryType;
 use Carbon\CarbonImmutable;
 
 /**
@@ -15,7 +16,7 @@ use Carbon\CarbonImmutable;
  *     date_added?: string|null,
  *     date_modified?: string|null,
  *     parent_id?: string|null,
- *     type?: string|null,
+ *     type: string,
  *     value?: string
  * }
  */
@@ -24,12 +25,12 @@ class CustomField extends BaseData implements Data
     public function __construct(
         public string $id,
         public string $category,
+        public CustomFieldCategoryType $type,
         public ?string $comment = null,
         public ?CarbonImmutable $date = null,
         public ?CarbonImmutable $date_added = null,
         public ?CarbonImmutable $date_modified = null,
         public ?string $parent_id = null,
-        public ?string $type = null,
         public mixed $value = null,
     ) {}
 
@@ -48,12 +49,12 @@ class CustomField extends BaseData implements Data
         return new self(
             id: $data['id'],
             category: $data['category'],
+            type: CustomFieldCategoryType::from($data['type']),
             comment: $data['comment'] ?? null,
             date: $date ? CarbonImmutable::parse($date) : null,
             date_added: $dateAdded ? CarbonImmutable::parse($dateAdded) : null,
             date_modified: $dateModified ? CarbonImmutable::parse($dateModified) : null,
             parent_id: $data['parent_id'] ?? null,
-            type: $data['type'] ?? null,
             value: $data['value'] ?? null,
         );
     }
