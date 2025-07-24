f99692c5d3c4b03da2e0679e90a8eb9d1558e23a
marielsabornido <marielsabornido@gmail.com>
2025-07-24 14:25:34 +0800

    add CustomFieldCategoryType enum

---
+++ src/Enums/CustomFieldCategoryType.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Blackbaud\Enums;
+
+enum CustomFieldCategoryType: string
+{
+    case Text = 'Text';
+    case Date = 'Date';
+    case Currency = 'Currency';
+    case Boolean = 'Boolean';
+    case CodeTableEntry = 'CodeTableEntry';
+    case ConstituentId = 'ConstituentId';
+    case FuzzyDate = 'FuzzyDate';
+}
