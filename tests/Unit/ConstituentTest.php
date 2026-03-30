61b60ad7147115dfe75a34e484229ac51c5f9410
marielsabornido <marielsabornido@gmail.com>
2026-03-30 21:06:48 +0800

    fix constituent search dto

--- tests/Unit/ConstituentTest.php
+++ tests/Unit/ConstituentTest.php
@@ -63,9 +63,11 @@ it('can search for constituents', function () use ($client) {
         ->and($results->count)->toBe(2)
         ->and($results->value)->toHaveCount(2)
         ->and($results->value[0])->toBeInstanceOf(ConstituentSearchResult::class)
+        ->and($results->value[0])->toBeInstanceOf(ConstituentSearchResult::class)
         ->and($results->value[0]->id)->toBe('280')
         ->and($results->value[0]->name)->toBe('Dr. Robert C. Hernandez')
-        ->and($results->value[0]->primary_email)->toBe('robertcarloshernandez@gmail.com')
+        ->and($results->value[0]->email)->toBe('robertcarloshernandez@gmail.com')
+        ->and($results->value[0]->fundraiser_status)->toBe('Active')
         ->and($results->value[1]->id)->toBe('412')
-        ->and($results->value[1]->last_name)->toBe('Hernandez');
+        ->and($results->value[1]->name)->toBe('Maria Hernandez');
 });
