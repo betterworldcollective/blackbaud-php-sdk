5cfea7930998a58c934fb1b11aadf105b52bc639
Ben <ben.yobp@betterworldcollective.com>
2026-04-07 00:59:18 -0400

    add list phones capability

--- src/Resources/ConstituentPhoneResource.php
+++ src/Resources/ConstituentPhoneResource.php
@@ -2,14 +2,40 @@
 
 namespace Blackbaud\Resources;
 
+use Blackbaud\Data\ApiCollection;
+use Blackbaud\Data\Constituent\Phone;
 use Blackbaud\Exceptions\BadRequestException;
+use Blackbaud\Exceptions\InvalidDataException;
 use Blackbaud\Exceptions\UnauthorizedException;
 use Blackbaud\Requests\Constituent\CreateConstituentPhone;
+use Blackbaud\Requests\Constituent\ListConstituentPhones;
 use Blackbaud\Requests\Constituent\UpdateConstituentPhone;
 use Saloon\Http\BaseResource;
 
 class ConstituentPhoneResource extends BaseResource
 {
+    /**
+     * @return ApiCollection<Phone>
+     *
+     * @throws BadRequestException
+     * @throws InvalidDataException
+     * @throws UnauthorizedException
+     *
+     * @see https://developer.sky.blackbaud.com/api#api=56b76470069a0509c8f1c5b3&operation=ListConstituentPhonesByConstituent
+     */
+    public function list(int $constituentId): ApiCollection
+    {
+        $phones = $this->connector->send(
+            new ListConstituentPhones($constituentId)
+        )->dto();
+
+        if (! $phones instanceof ApiCollection) {
+            throw new InvalidDataException('Invalid data found.');
+        }
+
+        return $phones;
+    }
+
     /**
      * @param  array<string, mixed>  $properties
      * @return int The ID of the newly created constituent phone returned by the API response
